9890df56e9e5fa2a3ddadd3663afb15fd0bebbc5
garethbilaney <gareth.bilaney@hhu.de>
2021-04-30 13:29:51 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
-import * as styles from './shared/styles';
-import * as global from './shared/global';
-import * as animation from './shared/animation';
-import * as icons from './shared/icons';
-
-export { styles, global, animation, icons };
-
-export * from './Avatar';
-export * from './Badge';
-export * from './Button';
-export * from './Icon';
-export * from './Link';
-
-/*import React from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
@@ -28,4 +15,3 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
-*/
